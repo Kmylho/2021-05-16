1fc0595a2438e4ce8c133c7f8ee4e2c3e39663cd
Andrés Camilo Piratova Pineda <52338707+Kmylho@users.noreply.github.com>
2021-05-16 17:34:52 -0500

    Segundo Test

--- Ejercicio_POO.py
+++ Ejercicio_POO.py
@@ -7,24 +7,36 @@ class Estudiante ():
         self.Programa=programa
         self.Semestre=semestre
 
-    def Visualizar(self):
+    def VisualizarEstudiante(self):
         print("Nombre: ",self.Nombre," Edad: ",self.Edad," Dirección: ",self.Direccion," Teléfono: ",self.Telefono," Programa: ",self.Programa," Semestre: ",self.Semestre)
     
 
 class Asignatura (Estudiante):
-    def __init__(self, nombre_asignatura, semestre_asignatura, lista_estudiante, nombre, edad, direccion, telefono, programa, semestre):
-        super().__init__(nombre, edad, direccion, telefono, programa, semestre)
+    def __init__(self, nombre_asignatura, semestre_asignatura, lista_estudiante):
         self.Nombre_Asignatura=nombre_asignatura
         self.Semestre_Asignatura=semestre_asignatura
         self.Lista_Estudiante=lista_estudiante
 
-    def Agregar(self):
-        print("Agregar estudiante")
+    def Agregar(self, nombre, edad, direccion, telefono, programa, semestre):
+        super().__init__(nombre,edad,direccion,telefono,programa,semestre)
+        print("Estudiante: ",self.Nombre," agregado a: ",self.Nombre_Asignatura)
 
     def Visualizar(self):
-        super().Visualizar()
+        print("Nombre Asignatura: ",self.Nombre_Asignatura," Semestre Asignatura: ",self.Semestre_Asignatura," Lista Estudiante: ",self.Lista_Estudiante)
+        
+    def Visualizar_Todo(self):
+        super().VisualizarEstudiante()
         print("Nombre Asignatura: ",self.Nombre_Asignatura," Semestre Asignatura: ",self.Semestre_Asignatura," Lista Estudiante: ",self.Lista_Estudiante)
 
 
-Estudiante1=Asignatura("Electiva 2",8,21,"x",24,"x",123,"Sistemas",8)
-Estudiante1.Visualizar()
+Asignatura1=Asignatura("Multimedia",8,21)
+Asignatura1.Visualizar()
+
+Asignatura2=Asignatura("electiva 2",8,21)
+Asignatura2.Visualizar()
+
+Estudiante1=Estudiante("Andres",21,"suba",132,"Sistemas",8)
+Estudiante1.VisualizarEstudiante()
+
+Asignatura2.Agregar("Camilo",21,"suba",123,"Sistemas",8)
+Asignatura2.Visualizar_Todo()
